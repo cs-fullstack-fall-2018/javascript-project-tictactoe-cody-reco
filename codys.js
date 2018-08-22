f24527bd5e37aff479648b14a205c8bdfffb4727
Christopher Pace <Christopher_pace@ymail.com>
2018-08-22 13:42:25 -0500

    another commit

--- codys.js
+++ codys.js
@@ -1,4 +1,16 @@
-function buttonClick(boardArea){
-    console.log("button clicked");
-    document.getElementById(boardArea).innerText="X"
+// function buttonClick(){
+//     console.log("button clicked");
+//  //   document.getElementById().innerText="X"
+// }
+var squares= document.querySelectorAll(".tsquare");
+
+console.log("squares.length " +squares.length);
+
+for (var i =0; i < squares.length; i++)
+{
+    squares[i].addEventListener("click", buttonClick);
 }
+
+function buttonClick() {
+    console.log("box clicked");
+    }
