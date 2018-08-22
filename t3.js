429d535e7749ba1a5f7a5f7162fde99e973df138
Mareco Nabors <naborsmarico99@gmail.com>
2018-08-22 11:01:37 -0500

    no change

--- t3.js
+++ t3.js
@@ -1,19 +1,42 @@
 
 // Setup your Variables
+var cirlce = "O";
+var X = "X";
+
 
 // Handle Restart game button
+var reset = document.getElementById("reset").reset();
 
 // Grab all the play spaces in the board
+var td = document.querySelectorAll(".horizontal");
+
 
 // Clear all the play spaces in the board
 
+
+
 // Setup click handlers for each play space on the board
+document.getElementById("")
+
 
 // Handle a play space being clicked
 
+
+
 // Switch player turn
+
+
+
 
 // Check for winner or CAT/tie
 
+function winner() {
+alert("YOU WIN")
+}
 
+winner();
 
+function CAT() {
+alert("TIE")
+}
+CAT();
